e1938ae3d15c9161f1b85846c05f9319d7d93f5b
Dan <d9a9n9i9e9l9@hotmail.com>
2019-05-24 01:38:59 -0400

    Rounded boxes, connected to firebase
    Wooo

--- app/src/main/java/com/example/messagesinkotlin/LoginActivity.kt
+++ app/src/main/java/com/example/messagesinkotlin/LoginActivity.kt
@@ -1,13 +1,38 @@
 package com.example.messagesinkotlin
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        signInButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = accountLoginPassword.text.toString()
+
+            Log.d("Login", "Attempt login with email/password: $email/****")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                /*.addOnCompleteListener()
+                .add*/
+        }
+
+        //User needs an account
+        accountExistsTextView.setOnClickListener{
+            Log.d("LoginActivity", "Try to show main activity")
+
+            //Launch login activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/messagesinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/messagesinkotlin/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,18 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         //Make button do stuff
         registerButton.setOnClickListener{
-            //Easier accessing of textboxes on login screen
-            val email = emailEditText.text.toString();
-            val username = usernameEditText.text.toString();
-            val password = passwordEditText.text.toString();
-
-            Log.d("MainActivity", "Email is: " + email)
-            Log.d("MainActivity", "Password: $password")   //$ does string format
-
-            //Firebase goes here
+            registerUser()  //Reformatted my code
         }
 
-        //User has account?
+        //User has account
         existingAccountTextView.setOnClickListener{
             Log.d("MainActivity", "Try to show login activity")
 
@@ -33,5 +27,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    //Reformatted for cleanliness
+    private fun registerUser() {
+        //Easier accessing of textboxes on login screen
+        val email = emailEditText.text.toString();
+        val username = usernameEditText.text.toString();
+        val password = passwordEditText.text.toString();
+
+        if (email.isEmpty() || password.isEmpty()){ //Requires password & email
+            Toast.makeText(this, "Please enter text in fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is: " + email)
+        Log.d("MainActivity", "Password: $password")   //$ does string format
+
+        //Firebase goes here
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if(!it.isSuccessful)    //Unsuccessful
+                    return@addOnCompleteListener
+
+                //Successful
+                Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Main", "Failed to create user: ${it.message}")   //Gives better message when user is not made
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
